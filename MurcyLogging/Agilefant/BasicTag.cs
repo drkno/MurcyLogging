4c74e7626bde5e70ca5737fe5556863643a62d27
Jay Harris <jay.harris@outlook.co.nz>
2015-05-13 15:33:13 +1200

    Adds a completely undocumented 'description' class for getting those pesky descriptions for AF

---
+++ MurcyLogging/Agilefant/BasicTag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MurcyLogging.Agilefant
+{
+    public class BasicTag : IAgilefantTag
+    {
+        public Tag Tag { get; set; }
+        public string Content { get; set; }
+
+        public string Serialize()
+        {
+            return string.Format("#{0} {1}", Tag.ToString().ToLower(), Content);
+        }
+    }
+}
---
+++ MurcyLogging/Agilefant/MultiTag.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MurcyLogging.Agilefant
+{
+    public class MultiTag : IAgilefantTag
+    {
+        public Tag Tag { get; set; }
+        public List<string> InfoPieces { get; private set; }
+
+        public MultiTag()
+        {
+            InfoPieces = new List<string>();
+        }
+
+        public string AddInfo(object o)
+        {
+            InfoPieces.Add(o.ToString());
+            return o.ToString();
+        }
+
+        public bool RemoveInfo(object o)
+        {
+            return InfoPieces.Remove(o.ToString());
+        }
+
+        public string Serialize()
+        {
+            var tagStart = "#" + Tag.ToString().ToLower();
+            var builder = new StringBuilder();
+
+            foreach (var piece in InfoPieces)
+            {
+                if (builder.Length != 0)
+                    builder.Append(",");
+                builder.Append(piece);
+            }
+
+            return string.Format("{0}[{1}]", tagStart, builder.ToString());
+        }
+    }
+}
